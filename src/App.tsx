a6c08b7b1c6c39e638e096c8eb10e626c6168701
ArunSinghGkm <arun@gkmit.co>
2022-09-13 16:40:47 +0530

    airbnb es-lint added

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hello world!
+        </p>
+        <p
+        >
+          This is a react project architecture with airbnb es-lint setup
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
